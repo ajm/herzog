5119f96a0a4d70dd37ce899152f43bc3b3d04178
Alan Medlar <ajm@gatsby.(none)>
2009-11-27 16:57:17 +0000

    pre-testing on servers, many bugs, still some issues with passing around variables for logging

---
+++ utils.py
@@ -0,0 +1,22 @@
+import logging
+import logging.handlers
+
+def get_logger(loggername, logdirname, logfilename, verbose) :
+
+    logname = "%s/%s.log" % (logdir, logfilename)
+    log = logging.getLogger(loggername)
+    log.setLevel(logging.DEBUG)
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG if verbose else logging.ERROR)
+    console.setFormatter(logging.Formatter("%(name)s:%(message)s"))
+
+    logfile = logging.handlers.RotatingFileHandler(logname, maxBytes=2**20, backupCount=5)
+    logfile.setLevel(logging.DEBUG)
+    logfile.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+
+    log.addHandler(console)
+    log.addHandler(logfile)
+
+    return log
+
