bd2c461fffc8d94a17ac92e9778a4436ca893ddd
Alan Medlar <ajm@gatsby.(none)>
2009-12-15 10:56:16 +0000

    hostnames in where.py

--- where.py
+++ where.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 import xmlrpclib
 
-p = xmlrpclib.ServerProxy('http://gatsby:8001')
+url = 'http://gatsby:8001'
+#url = 'http://euclid.kleta-lab:8001'
+
+p = xmlrpclib.ServerProxy(url)
 tmp = p.list_current()
 
 s = "%s\t%s:%s"
-#print s % ("fragment","hostname","remote dir")
-#print "-" * 100
 for k,v in tmp.items() :
     localpath,hostname = v
     print s % (localpath,hostname,k)
-
-#    print v,
-#    print "\t",
-#    print k
 
